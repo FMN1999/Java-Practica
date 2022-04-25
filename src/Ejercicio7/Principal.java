a4466259b8ef3d02d2b19238586719f93107fb54
Franco Navarro <franconavarro1889@gmail.com>
2022-04-25 17:08:31 -0300

    Ejercicio7

---
+++ src/Ejercicio7/Principal.java
@@ -0,0 +1,9 @@
+package Ejercicio7;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Menu m=new Menu();
+		m.start();			
+	}
+}
---
+++ src/logic/Login.java
@@ -0,0 +1,26 @@
+package logic;
+
+import java.util.LinkedList;
+import data.*;
+import entities.*;
+
+public class Login {
+	private DataPersona dp;
+	
+	public Login() {
+		dp=new DataPersona();
+	}
+	
+	public Persona validate(Persona p) {
+		return dp.getByUser(p);
+	}
+
+	public LinkedList<Persona> getAll(){
+		return dp.getAll();
+	}
+
+	public Persona getByDocumento(Persona per) {
+		return dp.getByDocumento(per);
+		
+	}
+}
